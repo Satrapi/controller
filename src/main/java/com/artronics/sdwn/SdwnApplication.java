35c51602b84d43c28de07d45e4c0345b22bb1c7b
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-07 02:09:49 +0100

    add spring web

--- src/main/java/com/artronics/sdwn/SdwnApplication.java
+++ src/main/java/com/artronics/sdwn/SdwnApplication.java
@@ -1,31 +1,20 @@
 package com.artronics.sdwn;
 
 import com.artronics.sdwn.config.SdwnBeanConfig;
-import com.artronics.sdwn.config.SdwnTaskExecutorConfig;
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-public class SdwnApplication {
-
-	public static void main(String[] args) {
-//        new SpringApplicationBuilder()
-//                .bannerMode(Banner.Mode.OFF)
-//                .child(SdwnApplication.class)
-//                .sources(SdwnBeanConfig.class)
-//                .run(args);
-//        new SpringApplicationBuilder()
-//                .bannerMode(Banner.Mode.OFF)
-//                .sources(SdwnBeanConfig.class)
-//                .child(SdwnApplication.class)
-//                .run(args);
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
-                SdwnBeanConfig.class,
-                SdwnTaskExecutorConfig.class);
-        context.start();
-	}
+@SpringBootApplication
+public class SdwnApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplicationBuilder builder = new SpringApplicationBuilder();
+        builder.sources(SdwnApplication.class, SdwnBeanConfig.class)
+               .build().run(args);
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+//                SdwnBeanConfig.class,
+//                SdwnTaskExecutorConfig.class);
+//        context.start();
+    }
 }
