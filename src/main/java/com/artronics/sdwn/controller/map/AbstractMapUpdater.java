61bed457b71175126a17784fceeb25861d3f6cb9
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-11 02:59:39 +0100

    abstract base map updater to supper class.

---
+++ src/main/java/com/artronics/sdwn/controller/map/AbstractMapUpdater.java
@@ -0,0 +1,88 @@
+package com.artronics.sdwn.controller.map;
+
+import com.artronics.sdwn.controller.log.NodeLogger;
+import com.artronics.sdwn.domain.entities.DeviceConnectionEntity;
+import com.artronics.sdwn.domain.entities.node.Neighbor;
+import com.artronics.sdwn.domain.entities.node.SdwnNodeEntity;
+import com.artronics.sdwn.domain.entities.packet.PacketEntity;
+import com.artronics.sdwn.domain.repositories.NodeRepo;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class AbstractMapUpdater implements MapUpdater
+{
+    protected final static Logger log = Logger.getLogger(AbstractMapUpdater.class);
+
+    @Autowired
+    protected NodeLogger nodeLogger;
+
+    protected SdwnNetworkMap<SdwnNodeEntity> networkMap;
+
+    protected WeightCalculator weightCalculator;
+
+    protected NodeRepo nodeRepo;
+
+    protected DeviceConnectionEntity device;
+
+    @Override
+    public SdwnNodeEntity addSink(DeviceConnectionEntity device)
+    {
+        log.debug("Update NetworkMap. Adding sink node: " + device.getSinkAddress() + " to " +
+                          "NetworkMap.");
+
+        SdwnNodeEntity sink = new SdwnNodeEntity(device.getSinkAddress(), device);
+        sink.setType(SdwnNodeEntity.Type.SINK);
+        sink.setStatus(SdwnNodeEntity.Status.ACTIVE);
+
+        sink = nodeRepo.save(sink);
+        log.debug("Sink Node persisted: " + sink.toString());
+        nodeLogger.newNode(sink);
+
+        networkMap.addNode(sink);
+
+        return sink;
+    }
+
+    @Autowired
+    public void setNetworkMap(SdwnNetworkMap<SdwnNodeEntity> networkMap)
+    {
+        this.networkMap = networkMap;
+    }
+
+    @Override
+    public NetworkMap<SdwnNodeEntity> getNetworkMap()
+    {
+        return networkMap;
+    }
+
+    @Autowired
+    public void setWeightCalculator(WeightCalculator weightCalculator)
+    {
+        this.weightCalculator = weightCalculator;
+    }
+
+    @Autowired
+    public void setNodeRepo(NodeRepo nodeRepo)
+    {
+        this.nodeRepo = nodeRepo;
+    }
+
+    protected class Report
+    {
+        protected final SdwnNodeEntity src;
+        protected final SdwnNodeEntity dst;
+        protected final Set<Neighbor> neighbors;
+
+        public Report(PacketEntity packet)
+        {
+            src = new SdwnNodeEntity(Integer.toUnsignedLong(packet.getSrcShortAdd()));
+            dst = new SdwnNodeEntity(Integer.toUnsignedLong(packet.getDstShortAdd()));
+            src.setDevice(packet.getDevice());
+            dst.setDevice(packet.getDevice());
+            neighbors = new HashSet<>(Neighbor.createNeighbors(packet));
+        }
+    }
+}
--- src/main/java/com/artronics/sdwn/controller/map/MapUpdaterImpl.java
+++ src/main/java/com/artronics/sdwn/controller/map/MapUpdaterImpl.java
@@ -1,60 +1,20 @@
 package com.artronics.sdwn.controller.map;
 
-import com.artronics.sdwn.controller.log.NodeLogger;
-import com.artronics.sdwn.domain.entities.DeviceConnectionEntity;
 import com.artronics.sdwn.domain.entities.node.Neighbor;
 import com.artronics.sdwn.domain.entities.node.Node;
 import com.artronics.sdwn.domain.entities.node.SdwnNodeEntity;
 import com.artronics.sdwn.domain.entities.packet.PacketEntity;
-import com.artronics.sdwn.domain.repositories.NodeRepo;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-import java.util.HashSet;
 import java.util.Set;
 
 @Component
 @Primary
-public class MapUpdaterImpl implements MapUpdater
+public class MapUpdaterImpl extends AbstractMapUpdater
 {
     private final static Logger log = Logger.getLogger(MapUpdaterImpl.class);
-
-    @Autowired
-    private NodeLogger nodeLogger;
-
-    private SdwnNetworkMap<SdwnNodeEntity> networkMap;
-
-//    private Map<Long,NetworkMap<SdwnNodeEntity>> netMap;
-//
-//    //This is the map associated with one device.
-//    private NetworkMap<SdwnNodeEntity> subMap;
-
-    private WeightCalculator weightCalculator;
-
-    private NodeRepo nodeRepo;
-
-    private DeviceConnectionEntity device;
-
-    @Override
-    public SdwnNodeEntity addSink(DeviceConnectionEntity device)
-    {
-        log.debug("Update NetworkMap. Adding sink node: " + device.getSinkAddress() + " to " +
-                          "NetworkMap.");
-
-        SdwnNodeEntity sink = new SdwnNodeEntity(device.getSinkAddress(), device);
-        sink.setType(SdwnNodeEntity.Type.SINK);
-        sink.setStatus(SdwnNodeEntity.Status.ACTIVE);
-
-        sink = nodeRepo.save(sink);
-        log.debug("Sink Node persisted: " + sink.toString());
-        nodeLogger.newNode(sink);
-
-        networkMap.addNode(sink);
-
-        return sink;
-    }
 
     @Override
     public void addPacket(PacketEntity packet)
@@ -127,50 +87,4 @@ public class MapUpdaterImpl implements MapUpdater
         return node;
     }
 
-    @Autowired
-    public void setNetworkMap(SdwnNetworkMap<SdwnNodeEntity> networkMap)
-    {
-        this.networkMap = networkMap;
-    }
-
-    @Override
-    public NetworkMap<SdwnNodeEntity> getNetworkMap()
-    {
-        return networkMap;
-    }
-//
-//    @Resource(name = "netMap")
-//    public void setNetMap(
-//            Map<Long, NetworkMap<SdwnNodeEntity>> netMap)
-//    {
-//        this.netMap = netMap;
-//    }
-
-    @Autowired
-    public void setWeightCalculator(WeightCalculator weightCalculator)
-    {
-        this.weightCalculator = weightCalculator;
-    }
-
-    @Autowired
-    public void setNodeRepo(NodeRepo nodeRepo)
-    {
-        this.nodeRepo = nodeRepo;
-    }
-
-    private class Report
-    {
-        private final SdwnNodeEntity src;
-        private final SdwnNodeEntity dst;
-        private final Set<Neighbor> neighbors;
-
-        public Report(PacketEntity packet)
-        {
-            src = new SdwnNodeEntity(Integer.toUnsignedLong(packet.getSrcShortAdd()));
-            dst = new SdwnNodeEntity(Integer.toUnsignedLong(packet.getDstShortAdd()));
-            src.setDevice(packet.getDevice());
-            dst.setDevice(packet.getDevice());
-            neighbors = new HashSet<>(Neighbor.createNeighbors(packet));
-        }
-    }
 }
