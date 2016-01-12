412afd77a2e9cc8349293f6e41438740da0682f2
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-13 00:09:34 +0100

    change SdnwMap test

--- src/test/java/com/artronics/sdwn/controller/map/SdwnNetworkMapTest.java
+++ src/test/java/com/artronics/sdwn/controller/map/SdwnNetworkMapTest.java
@@ -1,114 +1,96 @@
 package com.artronics.sdwn.controller.map;
 
-import com.artronics.sdwn.domain.entities.node.SdwnNode;
+import com.artronics.sdwn.controller.map.graph.GraphDelegator;
+import com.artronics.sdwn.controller.map.graph.SdwnGraphDelegator;
+import com.artronics.sdwn.controller.support.SeedNetworkGraph;
+import com.artronics.sdwn.domain.entities.node.SdwnNodeEntity;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class SdwnNetworkMapTest
 {
-//    NetworkMap networkMap;
-//
-//    SdwnNode node0;
-//    SdwnNode node1;
-//    SdwnNode node2;
-//    SdwnNode node3;
-//
-//    List<SdwnNode> nodes = new ArrayList<>();
-//
-//    @Before
-//    public void setUp() throws Exception
-//    {
-//        networkMap = new SdwnNetworkMap();
-//
-//        node0 = new SdwnNode(0L);
-//        node1 = new SdwnNode(1L);
-//        node2 = new SdwnNode(2L);
-//        node3 = new SdwnNode(3L);
-//
-//        networkMap.addNode(node0);
-//        networkMap.addNode(node1);
-//        networkMap.addNode(node2);
-//        networkMap.addNode(node3);
-//
-//        networkMap.addLink(node0, node1, 30);
-//        networkMap.addLink(node2, node1, 5);
-//        networkMap.addLink(node2, node0, 10);
-//        networkMap.addLink(node3, node1, 50);
-//
-//        nodes.add(node0);
-//        nodes.add(node1);
-//        nodes.add(node2);
-//        nodes.add(node3);
-//    }
-//
-//    @Test
-//    public void
-//    If_we_add_link_to_a_node_which_already_exists_it_should_ignore_it()
-//    {
-//        //null check on addLink otherwise jgrapht throws exp
-//        networkMap.addLink(node0, node2, 10);
-//    }
-//
-//    @Test
-//    public void It_should_return_true_if_a_node_hasLink_to_other_node()
-//    {
-//        assertThat(networkMap.hasLink(node0, node1), equalTo(true));
-//        //reverse dir
-//        assertThat(networkMap.hasLink(node1, node0), equalTo(true));
-//
-//        assertThat(networkMap.hasLink(node0, node3), equalTo(false));
-//
-//        assertFalse(networkMap.hasLink(node0, node3));
-//    }
-//
-//    @Test
-//    public void It_should_return_false_if_we_ask_a_node_hasLink_to_itself()
-//    {
-//        assertThat(networkMap.hasLink(node0, node0), equalTo(false));
-//    }
-//
-//    @Test
-//    public void Test_contains_node()
-//    {
-//        assertThat(networkMap.contains(node0), equalTo(true));
-//    }
-//
-//    @Test
-//    public void test_remove_node()
-//    {
-//        networkMap.removeNode(node3);
-//        assertFalse(networkMap.contains(node3));
-//    }
-//
-//    @Test
-//    public void test_remove_link(){
-//        networkMap.removeLink(node1,node2);
-//        assertFalse(networkMap.hasLink(node1,node2));
-//    }
-//    @Test
-//    public void test_remove_link_in_reverse(){
-//        networkMap.removeLink(node2,node1);
-//        assertFalse(networkMap.hasLink(node1,node2));
-//    }
-//
-//    @Test
-//    public void Two_nodes_with_same_address_are_equal()
-//    {
-//        SdwnNode eqNode0 = new SdwnNode(0L);
-//        assertThat(networkMap.contains(eqNode0), equalTo(true));
-//    }
-//
-//    @Test
-//    public void test_getAllNodes()
-//    {
-//        assertThat(networkMap.getAllNodes(), equalTo(nodes));
-//    }
+    private SeedNetworkGraph seeder = new SeedNetworkGraph();
+    private NetworkMap<SdwnNodeEntity> map;
+    private GraphDelegator<SdwnNodeEntity> graphDelegator;
+    private Graph<SdwnNodeEntity, DefaultWeightedEdge> graph;
+
+    private SdwnNodeEntity node135 = seeder.getNode135();
+    private SdwnNodeEntity node136 = seeder.getNode136();
+    private SdwnNodeEntity node137 = seeder.getNode137();
+
+
+    @Before
+    public void setUp() throws Exception
+    {
+        seeder.seed(false);
+
+        map = seeder.getNetworkMap();
+        graph = map.getNetworkGraph();
+
+        graphDelegator = new SdwnGraphDelegator(graph);
+    }
+
+    @Test
+    public void
+    If_we_add_link_to_a_node_which_already_exists_it_should_ignore_it()
+    {
+        //null check on addLink otherwise jgrapht throws exp
+        map.addLink(node136, node135, 10);
+    }
+
+    @Test
+    public void It_should_return_true_if_a_node_hasLink_to_other_node()
+    {
+        assertTrue(map.hasLink(node135,node136));
+        assertTrue(map.hasLink(node136,node135));
+
+        assertFalse(map.hasLink(node135,node137));
+    }
+
+    @Test
+    public void It_should_return_false_if_we_ask_a_node_hasLink_to_itself()
+    {
+        assertThat(map.hasLink(node135, node135), equalTo(false));
+    }
+
+    @Test
+    public void Test_contains_node()
+    {
+        assertThat(map.contains(node135), equalTo(true));
+    }
+
+    @Test
+    public void test_remove_node()
+    {
+        map.removeNode(node137);
+        assertFalse(map.contains(node137));
+    }
+
+    @Test
+    public void test_remove_link(){
+        map.removeLink(node136,node137);
+        assertFalse(map.hasLink(node137,node136));
+    }
+
+    @Test
+    public void Two_nodes_with_same_address_are_equal()
+    {
+        SdwnNodeEntity eqNode135= new SdwnNodeEntity(135L,seeder.getDevice1());
+        assertThat(map.contains(eqNode135), equalTo(true));
+    }
+
+    @Test
+    public void test_getAllNodes()
+    {
+        List<SdwnNodeEntity> nodes = map.getAllNodes();
+        assertThat(nodes.size(), equalTo(9));
+    }
 
 }
