f8282353c70338f6650cfe186c0168a296a4daf0
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-15 02:10:14 +0100

    impl node reg

--- src/main/java/com/artronics/sdwn/controller/remote/NodeRegistrationServiceImpl.java
+++ src/main/java/com/artronics/sdwn/controller/remote/NodeRegistrationServiceImpl.java
@@ -1,7 +1,9 @@
 package com.artronics.sdwn.controller.remote;
 
 import com.artronics.sdwn.domain.entities.node.SdwnNodeEntity;
+import com.artronics.sdwn.domain.repositories.NodeRepo;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,9 +11,18 @@ public class NodeRegistrationServiceImpl implements NodeRegistrationService
 {
     private final static Logger log = Logger.getLogger(NodeRegistrationServiceImpl.class);
 
+    private NodeRepo nodeRepo;
+
     @Override
     public SdwnNodeEntity registerNode(SdwnNodeEntity node)
     {
-        return null;
+        node.setStatus(SdwnNodeEntity.Status.IDLE);
+        return nodeRepo.persist(node);
+    }
+
+    @Autowired
+    public void setNodeRepo(NodeRepo nodeRepo)
+    {
+        this.nodeRepo = nodeRepo;
     }
 }
